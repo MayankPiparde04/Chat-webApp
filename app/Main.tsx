979f8e7511451ee9922dd632bd082b7b5e2363ef
Mayank Piparde <mayankpiparde20@gmail.com>
2024-06-04 22:05:53 +0530

    Initial Commit

---
+++ app/Main.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Main = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello1</Text>
+      <Text>Hello2</Text>
+      <Text>Hello3</Text>
+      <Text>Hello4</Text>
+      <Text>Hello5</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Main
+
+const styles = StyleSheet.create({})
---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Main from './Main'
+
+const index = () => {
+    return (
+        <View>
+            <Main />
+        </View>
+    )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
