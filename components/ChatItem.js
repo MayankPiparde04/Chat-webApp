5c684f516bba78707222b31ff845a78da1e1796a
Mayank Piparde <mayankpiparde20@gmail.com>
2024-06-08 19:56:29 +0530

    Day 5.1

--- components/ChatItem.js
+++ components/ChatItem.js
@@ -1,22 +1,49 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import React, { useContext } from 'react';
+// import {Image} from 'expo-image'
+import { UserContext } from '../context/UserContext';
 
-export default function ChatItem({ item }) {
+
+export default function ChatItem({ item, index }) {
+    const { state, dispatch } = useContext(UserContext);
+    // console.log(item?.profileUrl);
+
     return (
-        <TouchableOpacity className='flex-row  h-10 w-full bg-slate-500'>
-            <Text className=' text-red-600'>ChatItem</Text>
-            <Image
-                className='w-16 h-16 absolute justify-center items-center'
-                source={require('../assets/images/Icon.png')}
-                style={{ aspectRatio: 1 }}
-            />
-            <View className='flex-1 gap-1'>
-                <View className='flex-row justify-between'>
-
+        <TouchableOpacity>
+            <View
+                className='flex-row w-full 
+            items-center
+            px-2 py-1 m-px
+            bg-green-50'
+                style={{
+                    borderWidth: 1,
+                    borderColor: 'black'
+                }}>
+                <Image
+                    source={state?.users?.profileUrl}
+                    // source={require('../assets/images/Profilepic.jpg')}
+                    style={{
+                        height: hp(6),
+                        width: hp(6),
+                    }}
+                    className='rounded-full'
+                />
+                <View
+                    className='flex-1 ml-1'
+                    style={{
+                        borderWidth: 1,
+                        borderColor: 'red'
+                    }}>
+                    <View className='flex-row justify-between h-7 px-2 items-center'>
+                        <Text className='text-neutral-900 font-semibold'>Mayank</Text>
+                        <Text className='text-neutral-900 font-semibold'>Time</Text>
+                    </View>
+                    <View className='flex-row justify-start h-7 items-center px-2'>
+                        <Text className='text-neutral-900 font-semibold'>Last Message</Text>
+                    </View>
                 </View>
             </View>
         </TouchableOpacity>
     )
 }
-
-const styles = StyleSheet.create({})
--- components/ChatList.js
+++ components/ChatList.js
@@ -1,16 +1,22 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import React from 'react';
 import ChatItem from './ChatItem';
 
 export default function ChatList({ users }) {
+
+  console.log(users?.userId);
   return (
     <View clssName='flex-1'>
       <FlatList
         data={users}
-        contentContainerStyle={{ flex: 1, paddingVertical: 25 }}
+        contentContainerStyle={{ paddingVertical: 25 }}
         keyExtractor={item => Math.random()}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item, index }) => <ChatItem item={item} index={index} />}
+        renderItem={({ item, index }) => <ChatItem item={item} index={index}
+        />
+        }
+        className='h-full rounded-t-lg'
       />
     </View>
   );
